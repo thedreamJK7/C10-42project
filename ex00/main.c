cc3e2ce35efaa37da81c46839e0e6f09d35c9030
javokhirkubaev <jkubaev7@gmail.com>
2025-04-21 15:16:46 +0200

    finish

---
+++ ex00/main.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include <fcntl.h>
+
+void    ft_putstr(char *str);
+
+int main(int ac, char **av)
+{
+    int fl;
+    char c;
+
+    if (ac == 2)
+    {
+        fl = open(av[1], O_RDONLY);
+        if (!fl)
+            ft_putstr("Cannot read file.");
+        else
+        {
+            while (read(fl, &c, sizeof(c)) > 0)
+                write(1, &c, sizeof(c));
+        }
+        close(fl);
+    }
+    else if (ac > 2)
+    {
+        ft_putstr("Too many arguments.");
+    }
+    else
+    {
+        ft_putstr("File name is missing.");
+    }
+    return (0);
+}
